5968eec7fd5bf89bb5033a204cf3a36df8312155
Satoshi Iwasaki <yanqirenshi@gmail.com>
2016-01-16 11:09:49 +0900

    modify environment-at, add ensure.

--- src/tree/environment.lisp
+++ src/tree/environment.lisp
@@ -3,15 +3,22 @@
 ;;;;;
 ;;;;; Environment
 ;;;;;
-(defun environment-at (tree &key code application)
-  (assert tree)
+(defun %environment-at (tree code application)
   (cond ((and code application)
          (getf (find-if #'(lambda (node)
-                            (eq (code (getf node :vertex)) code))
+                            (eq (code (getf node :vertex)) env-code))
                         (find-r tree 'relationship :from application))
                :vertex))
-        (code (node-at tree 'environment :code code))
+        (code
+         (node-at tree 'environment :code code))
         (t nil)))
+
+(defun environment-at (tree &key code application ensure)
+  (assert tree)
+  (let ((env (%environment-at tree code application)))
+    (or env
+        (when (and code ensure)
+          (add-environment tree application code)))))
 
 (defun tx-make-environment (tree code &key (name ""))
   (assert tree)
